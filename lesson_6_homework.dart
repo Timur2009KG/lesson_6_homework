97d433cb87e25075553deec4f29da2fc7c69ee67
Timur2009KG <asanalievt93@gmail.com>
2023-05-27 15:01:58 +0300

    first commit

---
+++ lesson_6_homework.dart
@@ -0,0 +1,163 @@
+import 'dart:io';
+
+void main() { 
+  //#1
+  // num a = 5;
+  // num b = 5.126;
+  // num c = 0;
+  
+  // print(c = a+b);
+  // print(c = a-b);
+  // print(a+b.compareTo(c));
+  //я не знаю как это делать
+  
+  //#2
+  // num a = 5;
+  // num a1 = 2;   
+  // num b = 132;
+  // num b1 = 123;
+  // num c = 78;
+  // num c1 = 2;
+  // num c2 = 2.5;
+  // num d = 0;
+
+  // print("$a + $a1 = 7" );
+  // print("$a - $a1 = 3");
+  // print("$b * $b1 = 16236");
+  // print('$c / $c1 = 39');
+  // print('$c/$c2 = 31.2');
+  // print("$d*$a=0 ");
+
+  //#3
+  // я не знаю как это делать
+
+  //#4
+  // stdout.write("как тебя зовут? ");
+  // String name2 = stdin.readLineSync()!;
+
+  // stdout.write('ваш возраст: ');
+  // int age = int.parse(stdin.readLineSync()!);
+
+  // print("вас зовут $name2, ваш возраст $age");
+
+  //#5
+  // String Number = "+996220890928";
+
+  // if (Number.isEmpty) {
+  //   print("номер телефона не может быыть пустым");
+  // } else if (Number.length < 13 || Number.length > 13) {
+  //   print("заполните номер телефона правильно");
+  // } else {
+  //   print("ок поехали дальше");
+  // }
+
+  //#6
+  // bool isRain = false;
+
+  // if (isRain == true) {
+  //   print("возьми зонт ");
+  // } else if (isRain == false) {
+  //   print("не бери");
+  // }
+
+  
+  //#7
+  // num a = 10;
+  // if (a > 3){
+  //   print(a +10);
+  // }else{
+  //   print(a - 10);
+  // }
+  
+  
+  
+  //#8
+  // num a = 9;
+
+  // if(a < 7 ){
+  //   print('yes');
+  // }else if (a > 10){
+  //   print('no');
+  // }else if(a == 9){
+  //   print('error');
+  // }
+  
+  //#9
+  // num a = 8;
+  // num b = 9;
+  // if (a < b){
+  //   print(b);
+  // }else if (a>b){
+  //   print(a);
+  // }else{
+  //   print('error');
+  // }
+
+  //#10
+  // num a = 50;
+  // num b = 50;
+
+  // if (a + b == 100){
+  //   print('yes');
+  // }else if(a + b != 100){
+  //   print('no');
+  // }else{
+  //   print('error');
+  // }
+
+  //#11
+  // num a = 2;
+
+  // if (a == 1){
+  //   print('snowy');
+  // }else if(a == 2){
+  //   print('spring');
+  // }else if(a == 3){
+  //   print('sunny');
+  // }else if(a == 4){
+  //   print('outumn');
+  // }else{
+  //   print('error');
+  // }
+
+  //#12
+  // num a = 1;
+  // num b = 2;
+  // num c = 3;
+
+  // if (a < b || a<c ){
+  //   print(a);
+  // }else if(b < a || b<c){
+  //   print(b);
+  // }else if(c < a || c < b){
+  //   print(c);
+  // }else{
+  //   print("error");
+  // }
+
+  //#13
+  // String a = '123';
+
+  // print(a [0]);
+  // print(a [2]);
+
+  //#14
+  // я не знаю как это делать
+   
+  
+  //#15
+//   String a = '*';
+
+//   a*1;
+//   a*2;
+//   a*3;
+//   a*4;
+//   a*5;
+
+//  print(a*1); 
+//  print(a*2);
+//  print(a*3);
+//  print(a*4);
+//  print(a*5);
+  
+//я не знаю как делать 16 и 17
